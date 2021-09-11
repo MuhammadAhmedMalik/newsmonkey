1e424db6d7ac43d3b1332fb5f311202465ca4da3
Muhammad Ahmed <muhammadahmed7862468@gmail.com>
2021-09-11 23:54:18 +0500

    Categories working in Navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 export class App extends Component {
   render() {
     return (
       <div>
+        <Router>
         <NavBar/>
-        <News country="in" category="science"/>
+        <Switch>
+          <Route exact path="/home"><News key="general" country="us" category="general"/></Route>
+          <Route exact path="/business"><News key="business" country="us" category="business"/></Route>
+          <Route exact path="/entertainment"><News key="entertainment" country="us" category="entertainment"/></Route>
+          <Route exact path="/health"><News key="health" country="us" category="health"/></Route>
+          <Route exact path="/science"><News key="science" country="us" category="science"/></Route>
+          <Route exact path="/sports"><News key="sports" country="us" category="sports"/></Route>
+          <Route exact path="/technology"><News key="technology" country="us" category="technology"/></Route>
+        </Switch>
+        </Router>
       </div>
     )
   }
