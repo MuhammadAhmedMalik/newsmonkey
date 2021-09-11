e2e904d9742b66ce1e13321d1b0e0f36d310c464
Muhammad Ahmed <muhammadahmed7862468@gmail.com>
2021-09-11 21:40:17 +0500

    Next/Prev Button and loading spinner added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,12 +5,12 @@ export class NewsItem extends Component {
         let {title, description, imgUrl, newsUrl} = this.props;
         return (
             <div>
-                <div className="card" style={{width: "18rem"}}>
-                    <img src={imgUrl} className="card-img-top" alt="..."/>
-                    <div className="card-body">
+                <div className="card">
+                    <img src={!imgUrl?"https://t3.ftcdn.net/jpg/03/35/13/14/240_F_335131435_DrHIQjlOKlu3GCXtpFkIG1v0cGgM9vJC.jpg":imgUrl} className="card-img-top" alt="..."/>
+                    <div className="card-body">""
                         <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+                        <p className="card-text">{description}</p>
+                        <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
